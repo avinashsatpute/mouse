845cd5229c5e2834a9cc373f9b5902881e263607
avinashsatpute <63575511+avinashsatpute@users.noreply.github.com>
2022-03-31 16:35:41 +0530

    syntax_fixed

--- python.py
+++ python.py
@@ -1,8 +1,8 @@
 import os
 usr=input("enter string:")
 usr_cnf=input("enter yes or No if wants to convert you string to lowe case:")
-if usr_cnf == "yes"
+if usr_cnf == "yes":
  print(usr.lower())
-else
+else:
  print(usr)
   
